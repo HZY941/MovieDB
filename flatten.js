47e6d235535dc0d90282caa62f971bc91fb13f37
Zuyan He (Tim) <timmacy.he@gmail.com>
2022-05-30 13:07:34 -0700

    JS - Flatten

---
+++ flatten.js
@@ -0,0 +1,25 @@
+/**
+ * Flatten nested array items.
+ */
+
+const array = [1, 2, 3, [4, 5, 6, [7, 8], 9], 10];
+
+const flatten = (array) => {
+  const result = [];
+
+  const flattenElements = (array, result) => {
+    for (const item of array) {
+      if (Object.prototype.toString.call(item) === "[object Array]") {
+        flattenElements(item, result);
+      } else {
+        result.push(item);
+      }
+    }
+  };
+
+  flattenElements(array, result);
+  return result;
+};
+
+console.log("Original Array: ", array);
+console.log("Flattened: ", flatten(array));
