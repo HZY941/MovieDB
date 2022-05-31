6071cb6f3d956bdc23e572f321142b6a42bd2e68
Zuyan He (Tim) <timmacy.he@gmail.com>
2022-05-30 18:30:47 -0700

    Added JS - Check Palindrome

---
+++ isPalindrome.js
@@ -0,0 +1,11 @@
+/**
+ * Check if a given input string is palindrome or not.
+ *
+ * @input input string.
+ * @output booolean value that check if the string is palindrome or not.
+ */
+
+const str = "This is not";
+const str_2 = "hannah";
+
+const isPalindrome = () => {};
