4a7eefaca9046d177beaeb43e0f350189a720472
HZY941 <hezuyan0941@gmail.com>
2022-05-30 19:16:56 -0700

    Updated JS - Check Palindrome

--- isPalindrome.js
+++ isPalindrome.js
@@ -13,5 +13,18 @@ const isPalindrome = (str) => {
   return str === [...str].reverse().join("");
 };
 
+// solution 2 - check for half of the array with the other half
+const isPalindrome_v2 = (str) => {
+  const arr = [...str];
+  for (let i = 0; i < arr.length / 2; i++) {
+    if (arr[i] !== arr[arr.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 console.log(`'${str}' : ${isPalindrome(str)}`);
 console.log(`'${str_2}' : ${isPalindrome(str_2)}`);
+console.log(`V2: '${str}' : ${isPalindrome_v2(str)}`);
+console.log(`V2: '${str_2}' : ${isPalindrome_v2(str_2)}`);
